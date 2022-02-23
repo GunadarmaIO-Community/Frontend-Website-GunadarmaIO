3672926d9f82b8e1b6b0be8de44c84ed1afd7ba7
Gregorius Jimmy <gregoriusjimmy@gmail.com>
2022-02-23 19:08:23 +0700

    add axiosConfig, global styles, to the _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
-import '../styles/globals.css'
+import '../app/styles/app.css'
+import '../app/configs/axiosConfig'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
