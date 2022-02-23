507bd6d54f2632abdb706b7bf270c4136d45d5eb
Gregorius Jimmy <gregoriusjimmy@gmail.com>
2022-02-23 19:10:32 +0700

    add response and type files

---
+++ app/types/type.ts
@@ -0,0 +1,12 @@
+export type Certificate = {
+  id: string
+  no_sertifikat: string
+  nama: string
+  email: string
+  user_id: string
+  event_id: string
+  file: string
+  type: string
+  created_at: string
+  updated_at: string
+}
