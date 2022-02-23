49dd5e651bf648858a1ff74d12c7246ed62c58bc
Gregorius Jimmy <gregoriusjimmy@gmail.com>
2022-02-23 19:11:22 +0700

    add /certificates/index page

---
+++ pages/certificates/index.tsx
@@ -0,0 +1,45 @@
+import { Button, Input, Typography } from '@mui/material'
+import { Box } from '@mui/system'
+import { useState } from 'react'
+import { CertificatesLayout } from '../../app/layouts/Main/CertificateLayout'
+import { useRouter } from 'next/router'
+import Head from 'next/head'
+
+const CertificatesPage = () => {
+  const [input, setInput] = useState<string>('')
+  const router = useRouter()
+
+  return (
+    <>
+      <Head>
+        <title>UG I/O - Certificates</title>
+      </Head>
+      <CertificatesLayout>
+        <Box sx={{ textAlign: 'center' }}>
+          <Typography component='h1' mb={4} variant='h6'>
+            Validate a certificate from us
+          </Typography>
+          <Box sx={{ mb: 2 }}>
+            <Input
+              value={input}
+              inputProps={{ style: { textAlign: 'center' } }}
+              onChange={(e) => setInput(e.target.value)}
+              required
+              placeholder='Copy & paste your certificate serial number here'
+              fullWidth
+              sx={{ fontWeight: 700 }}
+            />
+          </Box>
+          <Button
+            variant='contained'
+            fullWidth
+            onClick={() => input && router.push(`/certificates/${input}`)}>
+            SEARCH
+          </Button>
+        </Box>
+      </CertificatesLayout>
+    </>
+  )
+}
+
+export default CertificatesPage
