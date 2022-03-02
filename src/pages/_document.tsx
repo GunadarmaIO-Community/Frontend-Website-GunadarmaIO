b34cdad9e906546b9fa558b0a44459f9d09a610b
gregoriusjimmy <gregoriusjimmy@gmail.com>
2022-03-02 21:15:08 +0700

    update tailwind config, add font

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,24 +4,23 @@ import Document, {
   Html,
   Main,
   NextScript,
-} from 'next/document';
+} from 'next/document'
 
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
-    const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps };
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
   }
 
   render() {
     return (
       <Html lang='en'>
         <Head>
+          <link rel='preconnect' href='https://fonts.googleapis.com' />
+          <link rel='preconnect' href='https://fonts.gstatic.com' />
           <link
-            rel='preload'
-            href='/fonts/inter-var-latin.woff2'
-            as='font'
-            type='font/woff2'
-            crossOrigin='anonymous'
+            href='https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,700;1,400;1,700&display=swap'
+            rel='stylesheet'
           />
         </Head>
         <body>
@@ -29,8 +28,8 @@ class MyDocument extends Document {
           <NextScript />
         </body>
       </Html>
-    );
+    )
   }
 }
 
-export default MyDocument;
+export default MyDocument
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,34 @@ module.exports = {
       fontFamily: {
         primary: ['Inter', ...fontFamily.sans],
       },
-      colors: {},
+      colors: {
+        primary: {
+          100: '#cce2fa',
+          200: '#99c5f5',
+          300: '#66a8f0',
+          400: '#338beb',
+          500: '#006ee6',
+          600: '#0058b8',
+          700: '#00428a',
+          800: '#002c5c',
+          900: '#00162e',
+        },
+        secondary: {
+          100: '#f9fcff',
+          200: '#f4f9ff',
+          300: '#eef7ff',
+          400: '#e9f4ff',
+          500: '#e3f1ff',
+          600: '#b6c1cc',
+          700: '#889199',
+          800: '#5b6066',
+          900: '#2d3033',
+        },
+        fontFamily: {
+          sans: ['Montserrat', 'sans-serif'],
+          serif: ['Montserrat', 'serif'],
+        },
+      },
       keyframes: {
         flicker: {
           '0%, 19.999%, 22%, 62.999%, 64%, 64.999%, 70%, 100%': {
