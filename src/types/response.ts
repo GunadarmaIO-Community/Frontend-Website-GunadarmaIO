d0dff8dc615e009a775593aedb10348fe4d5df73
gregoriusjimmy <gregoriusjimmy@gmail.com>
2022-03-04 08:47:33 +0700

    modify GetCertificateResponse

--- src/types/response.ts
+++ src/types/response.ts
@@ -1,14 +1,6 @@
+import { Certificate } from './type'
 export type GetCertificateResponse = {
-  data: {
-    id: string
-    no_sertifikat: string
-    nama: string
-    email: string
-    user_id: string
-    event_id: string
-    file: string
-    type: string
-    created_at: string
-    updated_at: string
-  }
+  success: boolean
+  message: string
+  data: Certificate[]
 }
