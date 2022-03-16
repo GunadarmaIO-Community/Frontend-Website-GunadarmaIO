9f2ed0b8d7bbcf8e6fb1d993e4d9a8e9a3b9fd36
gregoriusjimmy <gregoriusjimmy@gmail.com>
2022-03-16 07:21:47 +0700

    add Button variant props

--- src/components/elements/Button/Button.tsx
+++ src/components/elements/Button/Button.tsx
@@ -2,24 +2,45 @@ import { ButtonHTMLAttributes, forwardRef, ReactNode } from 'react'
 
 import clsxm from '@/lib/clsxm'
 
+enum ButtonVariant {
+  'primary',
+  'secondary',
+  'text',
+}
 interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: ReactNode
   className?: string
+  variant?: keyof typeof ButtonVariant
 }
 
-export const Button = forwardRef<HTMLButtonElement, Props>(
-  ({ children, className, ...rest }, ref) => {
-    return (
-      <button
-        {...rest}
-        ref={ref}
-        className={clsxm(
-          'rounded-md bg-primary-500 px-8 py-3 font-bold text-white',
-          className
-        )}
-      >
-        {children}
-      </button>
-    )
-  }
-)
+export const Button = forwardRef<HTMLButtonElement, Props>(({ children, className, variant, ...rest }, ref) => {
+  return (
+    <button
+      {...rest}
+      ref={ref}
+      className={clsxm(
+        'inline-flex items-center rounded px-4 py-2 font-semibold',
+        'focus:outline-none focus-visible:ring focus-visible:ring-primary-500',
+        'shadow-sm',
+        'transition-colors duration-75',
+        [
+          variant === 'primary' && [
+            'bg-primary-500 text-white',
+            'hover:bg-primary-600 hover:text-white',
+            'active:bg-primary-500',
+            'disabled:bg-primary-400 hover:disabled:bg-primary-400',
+          ],
+          variant === 'secondary' && [
+            'bg-secondary-500 text-primary-500 ',
+            'hover:bg-secondary-600',
+            'active:bg-secondary-500',
+            'disabled:bg-secondary-400  hover:disabled:bg-secondary-400',
+          ],
+        ],
+        className
+      )}
+    >
+      {children}
+    </button>
+  )
+})
