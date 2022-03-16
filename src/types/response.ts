77d3c6df027fca93e591cb87cfb398f2336738d8
gregoriusjimmy <gregoriusjimmy@gmail.com>
2022-03-16 10:39:13 +0700

    add DefaultResponse, GetDivisionResponse

--- src/types/response.ts
+++ src/types/response.ts
@@ -1,6 +1,14 @@
-import { Certificate } from './type'
-export type GetCertificateResponse = {
+import { Certificate, Division } from './type'
+
+export type DefaultResponse = {
   success: boolean
   message: string
+}
+
+export interface GetCertificateResponse extends DefaultResponse {
   data: Certificate[]
+}
+
+export interface GetDivisionResponse extends DefaultResponse {
+  data: Division[]
 }
