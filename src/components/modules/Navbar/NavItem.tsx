4385d6ffc66b339e41b0894a74fe0867c6fd40f3
falbas <farhanalbas30@gmail.com>
2022-04-08 21:53:54 +0700

    remove a tag from Link

--- src/components/modules/Navbar/NavItem.tsx
+++ src/components/modules/Navbar/NavItem.tsx
@@ -1,19 +1,16 @@
 import Link from 'next/link'
-import { HTMLAttributeAnchorTarget, ReactNode } from 'react'
+import { ReactNode } from 'react'
 
 type Props = {
   children: ReactNode
   href: string
   className?: string
-  target?: HTMLAttributeAnchorTarget
 }
 
-export const NavItem = ({ children, href, className, target }: Props) => {
+export const NavItem = ({ children, href, className }: Props) => {
   return (
-    <li className={`${className} mt-8 lg:ml-10 lg:mt-0 xl:ml-16 xl:mt-0`}>
-      <Link href={href}>
-        <a target={target}>{children}</a>
-      </Link>
+    <li className={`${className} mt-8 text-secondary-500 lg:ml-10 lg:mt-0 lg:text-black xl:ml-16 xl:mt-0`}>
+      <Link href={href}>{children}</Link>
     </li>
   )
 }
