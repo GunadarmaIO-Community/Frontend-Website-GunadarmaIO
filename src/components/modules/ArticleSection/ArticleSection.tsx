bbadaf6d9e00a0906fc3125e5f7ac173f2e5f16a
falbas <farhanalbas30@gmail.com>
2022-04-14 16:57:04 +0700

    fix ArticleSection layout

--- src/components/modules/ArticleSection/ArticleSection.tsx
+++ src/components/modules/ArticleSection/ArticleSection.tsx
@@ -10,37 +10,37 @@ type Props = {
 
 export const ArticleSection = ({ articles }: Props) => {
   return (
-    <div id='article' className='mt-28 flex flex-col'>
+    <section id='article' className='mt-20'>
       <h1 className='text-center font-bold'>
         Wanna boost your knowledge about <span className='text-primary-500'>algorithm</span> &{' '}
         <span className='text-primary-500'>data structure</span> ?
       </h1>
-      <p className='mt-3 mb-6 text-center'>Here are several resource that you can learn with</p>
-      <p className='text-right'>
+      <p className='mt-5 text-center'>Here are several resource that you can learn with</p>
+      <p className='mt-10 text-right'>
         <Link href='/'>
           <a className='font-bold text-primary-500 hover:opacity-75'>View All Articles</a>
         </Link>
       </p>
-      <div className='grid grid-cols-2 gap-4 md:grid-cols-3'>
+      <div className='mt-5 grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3'>
         {articles.map((article, index) => (
           <Link href={`article/${article.slug}`} key={index}>
-            <a>
-              <div className='rounded hover:opacity-75'>
-                <div className='w-full'>
-                  <NextImage
-                    className='relative h-[130px] w-full md:h-[200px]'
-                    src={article.gambar}
-                    layout='fill'
-                    objectFit='contain'
-                  />
-                </div>
-                <p className='font-bold text-primary-500'>{article.judul_artikel}</p>
+            <a className='flex flex-row rounded border p-3 shadow hover:opacity-75 sm:flex-col sm:p-5'>
+              <div className='w-1/3 sm:w-full'>
+                <NextImage
+                  className='relative min-h-[120px] w-full sm:h-[200px]'
+                  src={article.gambar}
+                  layout='fill'
+                  objectFit='contain'
+                />
+              </div>
+              <div className='w-2/3 pl-3 sm:w-full sm:pl-0'>
+                <p className='font-bold text-primary-500 sm:mt-3'>{article.judul_artikel}</p>
                 <p className='line-clamp-4'>{article.detail_singkat}</p>
               </div>
             </a>
           </Link>
         ))}
       </div>
-    </div>
+    </section>
   )
 }
