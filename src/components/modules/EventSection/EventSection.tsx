a233ee607fb22a9518f8d028f11dd127ed03f13c
falbas <farhanalbas30@gmail.com>
2022-04-14 16:20:00 +0700

    fix EventSection layout

--- src/components/modules/EventSection/EventSection.tsx
+++ src/components/modules/EventSection/EventSection.tsx
@@ -5,33 +5,6 @@ import { NextImage } from '@/elements/NextImage/NextImage'
 
 import { Event } from '@/types/type'
 
-const Slide = ({ event, className }) => {
-  return (
-    <div className={className}>
-      <div className='flex w-full flex-col lg:m-5 lg:flex-row'>
-        <div className='w-full lg:mr-5 lg:w-5/12'>
-          <NextImage
-            className='relative h-[200px] md:w-full lg:h-[250px]'
-            src={event.gambar}
-            layout='fill'
-            objectFit='contain'
-          />
-        </div>
-        <div className='mt-3 flex w-full flex-col gap-y-5 text-secondary-500 lg:w-7/12'>
-          <p>
-            March 13, 2022 <span className='text-[#54C659]'>(18:00 - 21:00 WIB)</span>
-          </p>
-          <h3 className='font-bold'>{event.judul_event}</h3>
-          <p>{event.detail_singkat}</p>
-          <div className='flex w-full shrink'>
-            <button className='rounded-[8px] bg-[#54C659] p-3 font-bold hover:opacity-75'>Join Event!</button>
-          </div>
-        </div>
-      </div>
-    </div>
-  )
-}
-
 type Props = {
   events: Event[]
 }
@@ -41,16 +14,19 @@ export const EventSection = ({ events }: Props) => {
   const [btnActive, setBtnActive] = useState(true)
   const [isRight, setIsRight] = useState(true)
 
-  const handleSlide = useCallback((i) => {
-    if (btnActive) {
-      setBtnActive(false)
-      setIsRight(i < 0 ? false : true)
-      setActive(active + i < 0 ? events.length - 1 : (active + i) % events.length)
-      setTimeout(() => {
-        setBtnActive(true)
-      }, 500)
-    }
-  }, [active, events.length, btnActive])
+  const handleSlide = useCallback(
+    (i) => {
+      if (btnActive) {
+        setBtnActive(false)
+        setIsRight(i < 0 ? false : true)
+        setActive(active + i < 0 ? events.length - 1 : (active + i) % events.length)
+        setTimeout(() => {
+          setBtnActive(true)
+        }, 500)
+      }
+    },
+    [active, events.length, btnActive]
+  )
 
   useEffect(() => {
     const timer = setInterval(() => (isRight ? handleSlide(1) : handleSlide(-1)), 3000)
@@ -58,38 +34,34 @@ export const EventSection = ({ events }: Props) => {
   }, [isRight, handleSlide])
 
   return (
-    <div id='events' className='mt-10 flex flex-col'>
-      <div className='flex justify-center'>
-        <div className='w-full xl:w-4/5'>
-          <h1 className='text-center font-bold'>
-            Get to know our <span className='text-primary-500'>events</span> and{' '}
-            <span className='text-primary-500'>participate</span> in it
-          </h1>
-          <p className='pt-5 text-center'>
-            Through our events, we aim to create a lasting impact for our participants in their journey of exploring the
-            vast world of technology
-          </p>
-        </div>
-      </div>
-      <div className='mt-10 flex justify-end'>
+    <section id='events' className='mt-20'>
+      <h1 className='text-center font-bold'>
+        Get to know our <span className='text-primary-500'>events</span> and{' '}
+        <span className='text-primary-500'>participate</span> in it
+      </h1>
+      <p className='mt-5 text-center'>
+        Through our events, we aim to create a lasting impact for our participants in their journey of exploring the
+        vast world of technology
+      </p>
+      <p className='mt-10 text-right'>
         <Link href='/'>
           <a className='font-bold text-primary-500 hover:opacity-75'>View All Events</a>
         </Link>
-      </div>
+      </p>
       <div className='mt-5 rounded-[24px] bg-gradient-to-b from-primary-500 to-primary-700'>
-        <div className='relative m-5 h-[420px] lg:h-[360px] overflow-hidden justify-center'>
+        <div className='relative h-[530px] justify-center overflow-hidden sm:h-[650px] lg:h-[370px]'>
           {events.map((event, index) => (
             <Slide
               key={index}
               event={event}
-              className={`absolute w-full h-full transition duration-1000 ease-in-out ${
-                index == active && 'block' ||
-                index == (active + 1) % events.length && 'translate-x-full opacity-0' ||
+              className={`absolute h-full w-full p-5 transition duration-1000 ease-in-out lg:p-8 ${
+                (index == active && 'block') ||
+                (index == (active + 1) % events.length && 'translate-x-full opacity-0') ||
                 '-translate-x-full opacity-0'
               }`}
             />
           ))}
-          <div className='absolute right-0 bottom-0'>
+          <div className='absolute right-5 bottom-5 lg:bottom-8 lg:right-8'>
             <button
               className='mr-12 rounded-full bg-secondary-500 p-3 hover:opacity-75'
               onClick={() => handleSlide(-1)}
@@ -115,6 +87,38 @@ export const EventSection = ({ events }: Props) => {
                 <path strokeLinecap='round' strokeLinejoin='round' d='M14 5l7 7m0 0l-7 7m7-7H3' />
               </svg>
             </button>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+type SlideProps = {
+  event: Event
+  className: string
+}
+
+const Slide = ({ event, className }: SlideProps) => {
+  return (
+    <div className={className}>
+      <div className='flex w-full flex-col lg:flex-row'>
+        <div className='w-full lg:w-5/12'>
+          <NextImage
+            className='relative h-[200px] sm:h-[300px] lg:h-[250px]'
+            src={event.gambar}
+            layout='fill'
+            objectFit='contain'
+          />
+        </div>
+        <div className='mt-3 flex w-full flex-col gap-y-5 text-secondary-500 lg:mt-0 lg:w-7/12 lg:pl-5'>
+          <p>
+            March 13, 2022 <span className='text-[#54C659]'>(18:00 - 21:00 WIB)</span>
+          </p>
+          <h3 className='font-bold'>{event.judul_event}</h3>
+          <p className='line-clamp-5'>{event.detail_singkat}</p>
+          <div className='flex w-full shrink'>
+            <button className='rounded-[8px] bg-[#54C659] p-3 font-bold hover:opacity-75'>Join Event!</button>
           </div>
         </div>
       </div>
